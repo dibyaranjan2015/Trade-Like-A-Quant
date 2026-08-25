203ed04b7a66b16461e51d9df0b0b60abc7ee826
dibyaranjan2015 <dibyaranjan.avanti@gmail.com>
2026-08-25 23:05:50 +0530

    day 2 link update

--- pages/2_Dot_Product.py
+++ pages/2_Dot_Product.py
@@ -23,7 +23,7 @@ nav_left, nav_right = st.columns(2)
 with nav_left:
     st.page_link("pages/1_Vectors.py", label="Back: Vectors")
 with nav_right:
-    st.link_button("Next: Matrices", "#") # to be updated post page3 is published
+    st.button("Next: Matrices", disabled = True) # to be updated post page3 is published
 
 # ---------------------------------------------------------------- header
 st.markdown(
