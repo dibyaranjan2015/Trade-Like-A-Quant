e44edb8db06975931e208853dc66d6645b837055
dibyaranjan2015 <dibyaranjan.avanti@gmail.com>
2026-08-28 15:23:08 +0530

    Day 1

--- core/registry.py
+++ core/registry.py
@@ -11,8 +11,8 @@ from core.day03_matrices import Matrices
 # (day_number, concept_instance, page_path)
 REGISTRY = [
     (1, Vectors(), "pages/1_Vectors.py"),
-    (2, DotProduct(), "pages/2_Dot_Product.py"),
-    (3, Matrices(), "pages/3_Matrices.py"),
+    # (2, DotProduct(), "pages/2_Dot_Product.py"),
+    # (3, Matrices(), "pages/3_Matrices.py"),
     # (4, LinearTransformations(), "pages/4_Linear_Transformations.py"),
 ]
 
